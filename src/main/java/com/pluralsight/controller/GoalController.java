62f0589d424abb343f3b3ad15ee7771afbca4c83
ysuresh81@gmail.com <s.satya.yellamelli@accenture.com>
2017-09-08 08:49:00 +0530

    Signed-off-by: ysuresh81@gmail.com <s.satya.yellamelli@accenture.com>

--- src/main/java/com/pluralsight/controller/GoalController.java
+++ src/main/java/com/pluralsight/controller/GoalController.java
@@ -1,7 +1,10 @@
 package com.pluralsight.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,14 +17,22 @@ import com.pluralsight.model.Goal;
 public class GoalController {
 
 	@RequestMapping(value = "addGoal", method = RequestMethod.GET)	
+	
 	public String addGoal(Model model) {
 		model.addAttribute("goal", new Goal());
 		return "addGoal";
 	}
+	
+	
 	@RequestMapping(value = "addGoal", method = RequestMethod.POST)	
-    public String updateGoal(@ModelAttribute("goal") Goal goal) {
+	
+    public String updateGoal(@Valid @ModelAttribute("goal") Goal goal, BindingResult result) {
 		
-		System.out.println("Goal minutes: "+goal.getMinutes());
+		System.out.println("Errors: "+result.hasErrors());
+		
+		if(result.hasErrors())
+			return "addGoal";
+		
 		return "redirect:addMinutes.html ";
 	}  
 
--- src/main/webapp/WEB-INF/jsp/addGoal.jsp
+++ src/main/webapp/WEB-INF/jsp/addGoal.jsp
@@ -6,23 +6,26 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
-
-
 <html>
 <head>
+
+<script type="text/css" src="src/main/webapp/WEB-INF/css/app.css"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Add Goal</title>
-</head>
-<body>
 
+</head>
+
+<body>
 	<form:form commandName="goal">
+		<form:errors  path="*" cssClass="errorblock" element="div" />
 		<table>
 			<tr>
 				<td>Enter Minutes:</td>
-				<td><form:input path="minutes" /></td>
+				<td><form:input path="minutes" cssErrorClass="error"/></td>
+				<td><form:errors cssClass="error" path="minutes"></form:errors>
 			</tr>
 			<tr>
-				<td colspan="2"><input type="submit" value="Enter Goal Minutes" /></td>
+				<td colspan="3"><input type="submit" value="Enter Goal Minutes" /></td>
 			</tr>
 
 		</table>
